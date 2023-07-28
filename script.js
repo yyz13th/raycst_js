588f5d791c95d900a213de1cb18fee68e7ac4084
yyz13th <136233684+yyz13th@users.noreply.github.com>
2023-07-28 11:52:19 +0300

    added minimap + player pos

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 
-const screenWidth = window.innerWidth,
-    screenHeight = window.innerHeight,
+const SCREEN_WIDTH = window.innerWidth,
+    SCREEN_HEIGHT = window.innerHeight,
     canvas = document.createElement('canvas');
 
-canvas.setAttribute('width', screenWidth);
-canvas.setAttribute('height', screenHeight);
+canvas.setAttribute('width', SCREEN_WIDTH);
+canvas.setAttribute('height', SCREEN_HEIGHT);
 document.body.appendChild(canvas); //puts canvas into body
 
 const tick = 30; //inits refresh rate
 const c = canvas.getContext('2d');
+const CELL_SIZE = 64;
+const PLAYER_SIZE = 10;
 
+const map = [
+    [1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 0, 1, 1],
+    [1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1],
+];
+
+const player = {
+    x:CELL_SIZE*1.5,
+    y:CELL_SIZE*2,
+    angle: 0,
+    speed: 0
+}
 function clearScreen(){
     c.fillStyle = 'blue';
-    c.fillRect(0, 0, screenWidth, screenHeight);
+    c.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 }
 
 function movePlayer(){
@@ -22,6 +40,7 @@ function movePlayer(){
 }
 
 function getRays(){
+    return [];
     
 }
 
@@ -29,8 +48,20 @@ function renderScene(){
     
 }
 
-function renderMinimap(x, y, scale, rays){
-    
+function renderMinimap(posX = 0, posY = 0, scale = 1, rays){
+    const cellSize = scale *CELL_SIZE;
+    map.forEach((row,y) => {
+        row.forEach((cell, x) => {
+            if(cell){
+            c.fillStyle = 'grey';
+            c.fillRect(posX+x*cellSize, posY+y*cellSize, cellSize, cellSize);
+            };
+        })
+    });
+
+    c.fillStyle = 'lime'
+    c.fillRect(posX + player.x *scale - PLAYER_SIZE/2,
+        posY + player.y *scale - PLAYER_SIZE/2, PLAYER_SIZE, PLAYER_SIZE);
 }
 
 
